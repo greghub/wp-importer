d3ac10f0244648a99a28312992e1e4a9680fd840
Greg Hovanesyan <connect.grigor@gmail.com>
2013-07-31 04:23:02 +0400

    Update xml_converter_for_wp.php

--- xml_converter_for_wp.php
+++ xml_converter_for_wp.php
@@ -7,7 +7,7 @@ www: http://sevenaristocrats.com/, https://github.com/greghub
 This script converts XML files into a format recognized by wp-importer. 
 It's used to quickly import XML data into wordpress as posts.
 
-git v1
+git v2
 */
 
 
